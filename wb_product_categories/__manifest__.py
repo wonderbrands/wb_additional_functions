32666b7a00570f018d1eba0a5e80f1de9f27e7d7
AnibalArenas <anibal@wonderbrands.co>
2022-10-24 14:10:32 -0500

    [UPD] Comenta campos a lista de empaque y agrega licencias LGPL-3

--- wb_product_categories/__manifest__.py
+++ wb_product_categories/__manifest__.py
@@ -21,7 +21,7 @@
 
     'author': "Wonderbrands",
     'website': "https://www.wonderbrands.co",
-
+    'license': 'LGPL-3',
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
